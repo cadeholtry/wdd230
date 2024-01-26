76bbf1b7cd4e0a56ead0545e930fefe4bc7cdade
cadeholtry <cvholtry@gmail.com>
2024-01-25 18:19:57 -0800

    03 assignment

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -1,3 +1,13 @@
 const currentyear = {year: 'numeric'};
 document.getElementById('currentyear').textContent = new Date().toLocaleDateString('en-US', currentyear);
 document.getElementById('lastModified').textContent = "Last Modification: " + document.lastModified;
+
+const hamButton = document.querySelector('#menu');
+const navigation = document.querySelector('nav');
+const map = document.querySelector('iframe');
+
+hamButton.addEventListener('click', () => {
+	navigation.classList.toggle('open');
+	hamButton.classList.toggle('open');
+    map.classList.toggle('open');
+});
