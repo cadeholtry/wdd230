1b2baa3f92af3176e80bb2de2ed0e066aae64602
cadeholtry <cvholtry@gmail.com>
2024-02-09 16:54:34 -0800

    05 assignment

---
+++ chamber/scripts/darkmode.js
@@ -0,0 +1,16 @@
+const modeButton = document.querySelector("#mode");
+const main = document.querySelector("main");
+
+modeButton.addEventListener("click", () => {
+	if (modeButton.textContent.includes("🕶️")) {
+		main.style.background = "#000";
+		main.style.color = "#fff";
+		modeButton.textContent = "🔆";
+	} else {
+		main.style.background = "#eee";
+		main.style.color = "#000";
+		modeButton.textContent = "🕶️";
+	}
+});
+// Any time I tried to mess with this it would break
+//bear with me and the minimal changes I made to this, its been hours
