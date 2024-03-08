f4555d6b99928342b61ca23db652fad0f7c96691
cadeholtry <cvholtry@gmail.com>
2024-03-07 18:02:30 -0800

    whoopd

--- scripts/links.js
+++ scripts/links.js
@@ -1,22 +1,28 @@
 const baseURL = "https://cadeholtry.github.io/wdd230/";
-const linksURL = `${baseURL}/data/links.js`;
+const linksURL = "https://cadeholtry.github.io/wdd230/data/links.json";
 const linkArea = document.querySelector("#activities");
 
 async function getLinks() {
     const response = await fetch(linksURL);
     const data = await response.json();
-    displayLinks(data);
+    displayLinks(data.lessons);
 }
 
-const displayLinks = (activities) => {
-    activities.forEach((activity) => {
+const displayLinks = (lessons) => {
+    lessons.forEach((activity) => {
         let lessons = document.createElement('section');
         let information = document.createElement('h4');
-        if ((activity.links).length == 1) {
-            activities.textContent = `Week ${activity.lesson}: <a href="${activity.links.url}" target="_blank">${activity.links.title}</a>`;
-        }
-        else {
-            activities.textContent = "balls";
+        information.textContent = `Week ${activity.lesson}: `;
+        let checker = (activity.links).length;
+        while (checker > 0) {
+            let link = document.createElement('a');
+            link.setAttribute('href', activity.links[checker - 1].url);
+            link.textContent = activity.links[checker - 1].title;
+            information.append(link);
+            if (checker > 1) {
+                information.append(' | ');
+            }
+            checker -= 1;
         }
         lessons.appendChild(information);
         linkArea.appendChild(lessons);
