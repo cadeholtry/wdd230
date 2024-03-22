108bfc0984d7b9d6e8a4c779443369fb2bbdfec5
cadeholtry <cvholtry@gmail.com>
2024-03-22 11:16:19 -0700

    Chamber final part 1

--- chamber/scripts/weather.js
+++ chamber/scripts/weather.js
@@ -26,19 +26,10 @@ else if (d.getDay() == 6) {
 }
 
 async function apiFetch() {
-    try {
-        const response = await fetch(url);
-        if (response.ok) {
-            const data = await response.json();
-            console.log(data);
-            displayResults(data);
-        }
-        else {
-            throw Error(await response.text());
-        }
-    }
-    catch (error) {
-        console.log(error);
+    const response = await fetch(url);
+    if (response.ok) {
+        const data = await response.json();
+        displayResults(data);
     }
 }
 
